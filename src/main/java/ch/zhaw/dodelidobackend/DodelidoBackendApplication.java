52f8f56eac2105eb25ec65bed989c13d7e5d1fb3
Nadine <dussnad1@students.zhaw.ch>
2021-05-27 20:38:46 +0200

    initial git project setup

---
+++ src/main/java/ch/zhaw/dodelidobackend/DodelidoBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.dodelidobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodelidoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodelidoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/zhaw/dodelidobackend/DodelidoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.dodelidobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodelidoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
