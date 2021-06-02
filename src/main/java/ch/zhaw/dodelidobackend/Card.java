3dd4b47dc4155f9334c9ae40b1a44e5bcd77ecc5
Nadine <dussnad1@students.zhaw.ch>
2021-06-02 07:45:48 +0200

    test class sfor ervice opencard class

--- src/main/java/ch/zhaw/dodelidobackend/Card.java
+++ src/main/java/ch/zhaw/dodelidobackend/Card.java
@@ -25,7 +25,29 @@ public class Card {
         this.animal = animal;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+       //Referenzgleichheheit
+        if (obj == this) {
+            return true;
+        }
+        if((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+        Card testCard = (Card) obj;
+        return (animal == testCard.getAnimal() && color == testCard.getColor());
+        }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 3;
+        result = prime * result
+                + ((color == null) ? 0 : color.hashCode());
+        result = prime * result
+                + ((animal == null) ? 0 : animal.hashCode());
+        return result;
+    }
     public enum Animals {TURTOISE, ZEBRA, FLAMINGO, CAMEL, PENGUIN}
     public enum Colors {WHITE, YELLOW, BLUE, RED, GREEN}
 }
---
+++ src/test/java/ch/zhaw/dodelidobackend/OpenCardServiceTest.java
@@ -0,0 +1,29 @@
+package ch.zhaw.dodelidobackend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OpenCardServiceTest {
+
+    private OpenCardService deck = new OpenCardService();
+
+    @Test
+    public void replaceCard() {
+        Card[] round1 = deck.getOpenCards2();
+        deck.replaceOneOpenCard2();
+        Card[] round2 = deck.getOpenCards2();
+
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            if(round1[i].equals(round2[i])) {
+                sum++;
+            }
+        }
+        //one card is replaced, 2 card should stay the same. it can happen that
+        // the replacement card is identical to its predecessor.
+        assertTrue(sum >= 2);
+    }
+
+
+}
